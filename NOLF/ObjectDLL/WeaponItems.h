2bcdaa03331f65810cdddbe8100c1d883f5304ea
BreakinBenny <fox@blaj.net>
2023-05-13 16:58:34 +0200

    Update WeaponItems.h

--- NOLF/ObjectDLL/WeaponItems.h
+++ NOLF/ObjectDLL/WeaponItems.h
@@ -1,6 +1,5 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : WeaponItems.h
+// MODULE: WeaponItems.h
 //
 // PURPOSE : Weapon items implementation
 //
@@ -9,7 +8,6 @@
 // REVISED : 10/22/99 - jrg
 //
 // (c) 1997-2001 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __WEAPON_ITEMS_H__
@@ -24,24 +22,24 @@ class WeaponItem : public PickupItem
 
 		WeaponItem();
 
-        uint8 GetWeaponId() const { return m_nWeaponId; }
-        uint8 GetAmmoId()	const { return m_nAmmoId; }
+		uint8 GetWeaponId()	const { return m_nWeaponId; }
+		uint8 GetAmmoId()	const { return m_nAmmoId; }
 		virtual void	PickedUp(HMESSAGEREAD hRead);
 
 		void SetIsLevelPowerup(LTBOOL b) { m_bIsLevelPowerup = b; }
 
 	protected :
 
-        uint32          EngineMessageFn(uint32 messageID, void *pData, LTFLOAT fData);
+		uint32	EngineMessageFn(uint32 messageID, void *pData, LTFLOAT fData);
 
-        uint8           m_nWeaponId;
-        uint8           m_nAmmoId;
-		int				m_nAmmo;
+		uint8	m_nWeaponId;
+		uint8	m_nAmmoId;
+		int	m_nAmmo;
 
 		// NOTE:  m_bLevelPowerup was added for Update 1.002 and is NOT saved 
 		// because it only changes the behavoir of multiplayer games.
 
-		LTBOOL			m_bIsLevelPowerup;
+		LTBOOL	m_bIsLevelPowerup;
 
 		virtual void	ObjectTouch (HOBJECT hObject);
 
@@ -51,21 +49,21 @@ class WeaponItem : public PickupItem
 		void PostPropRead(ObjectCreateStruct *pStruct);
 		void InitialUpdate();
 
-        void Save(HMESSAGEWRITE hWrite, uint32 dwSaveFlags);
-        void Load(HMESSAGEREAD hRead, uint32 dwLoadFlags);
+		void Save(HMESSAGEWRITE hWrite, uint32 dwSaveFlags);
+		void Load(HMESSAGEREAD hRead, uint32 dwLoadFlags);
 };
 
 class CWeaponItemPlugin : public IObjectPlugin
 {
 	public:
 
-        virtual LTRESULT PreHook_EditStringList(
+		virtual LTRESULT PreHook_EditStringList(
 			const char* szRezPath,
 			const char* szPropName,
 			char** aszStrings,
-            uint32* pcStrings,
-            const uint32 cMaxStrings,
-            const uint32 cMaxStringLength);
+			uint32* pcStrings,
+			const uint32 cMaxStrings,
+			const uint32 cMaxStringLength);
 
 	private:
 
