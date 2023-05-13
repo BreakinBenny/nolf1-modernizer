c0f348d0ff73855aec5ec4d786b09f9349bc1d4c
BreakinBenny <fox@blaj.net>
2023-05-14 01:07:19 +0200

    Update DeathScene.h

--- NOLF/ObjectDLL/DeathScene.h
+++ NOLF/ObjectDLL/DeathScene.h
@@ -1,11 +1,9 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : CDeathScene.h
+// MODULE: CDeathScene.h
 //
 // PURPOSE : Target information encapsulation class
 //
 // CREATED : 2/16/99
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __DEATH_SCENE_H__
@@ -22,26 +20,26 @@ class CDeathScene
 			m_eCharacterDeath = CD_NORMAL;
 			m_eCharacterClass = UNKNOWN;
 
-            m_hSceneObject = LTNULL;
+			m_hSceneObject	= LTNULL;
 
-			m_fNoiseVolume = 0.0f;
-			m_fNoiseTime = 0.0f;
+			m_fNoiseVolume	= 0.0f;
+			m_fNoiseTime	= 0.0f;
 
 			m_fLastPainTime		= -(float)(INT_MAX);
 			m_fLastPainVolume	= 0.0f;
 		}
 
-        LTFLOAT GetNoiseVolume() { return m_fNoiseVolume; }
-        LTFLOAT GetNoiseTime() { return m_fNoiseTime; }
+		LTFLOAT GetNoiseVolume() { return m_fNoiseVolume; }
+		LTFLOAT GetNoiseTime() { return m_fNoiseTime; }
 
-        LTFLOAT GetLastPainVolume() { return m_fLastPainVolume; }
-        LTFLOAT GetLastPainTime() { return m_fLastPainTime; }
+		LTFLOAT GetLastPainVolume() { return m_fLastPainVolume; }
+		LTFLOAT GetLastPainTime() { return m_fLastPainTime; }
 
-        LTVector GetPosition()
+		LTVector GetPosition()
 		{
-           LTVector vPos;
-           g_pLTServer->GetObjectPos(m_hSceneObject, &vPos);
-		   return vPos;
+			LTVector vPos;
+			g_pLTServer->GetObjectPos(m_hSceneObject, &vPos);
+			return vPos;
 		}
 
 		HOBJECT GetObject() { return m_hSceneObject; }
@@ -58,28 +56,28 @@ class CDeathScene
 
 		void Save(HMESSAGEWRITE hWrite)
 		{
-            if ( !hWrite || !g_pLTServer ) return;
+			if ( !hWrite || !g_pLTServer ) return;
 
-            g_pLTServer->WriteToMessageDWord(hWrite, m_eCharacterDeath);
-            g_pLTServer->WriteToMessageDWord(hWrite, m_eCharacterClass);
-            g_pLTServer->WriteToLoadSaveMessageObject(hWrite, m_hSceneObject);
-            g_pLTServer->WriteToMessageFloat(hWrite, m_fNoiseVolume);
-            g_pLTServer->WriteToMessageFloat(hWrite, m_fNoiseTime);
-            g_pLTServer->WriteToMessageFloat(hWrite, m_fLastPainTime);
-            g_pLTServer->WriteToMessageFloat(hWrite, m_fLastPainVolume);
+			 g_pLTServer->WriteToMessageDWord(hWrite, m_eCharacterDeath);
+			g_pLTServer->WriteToMessageDWord(hWrite, m_eCharacterClass);
+			g_pLTServer->WriteToLoadSaveMessageObject(hWrite, m_hSceneObject);
+			g_pLTServer->WriteToMessageFloat(hWrite, m_fNoiseVolume);
+			g_pLTServer->WriteToMessageFloat(hWrite, m_fNoiseTime);
+			g_pLTServer->WriteToMessageFloat(hWrite, m_fLastPainTime);
+			g_pLTServer->WriteToMessageFloat(hWrite, m_fLastPainVolume);
 		}
 
 		void Load(HMESSAGEREAD hRead)
 		{
-            if ( !hRead || !g_pLTServer ) return;
+			if ( !hRead || !g_pLTServer ) return;
 
-            m_eCharacterDeath = (CharacterDeath)g_pLTServer->ReadFromMessageDWord(hRead);
-            m_eCharacterClass = (CharacterClass)g_pLTServer->ReadFromMessageDWord(hRead);
-            g_pLTServer->ReadFromLoadSaveMessageObject(hRead, &m_hSceneObject);
-            m_fNoiseVolume = g_pLTServer->ReadFromMessageFloat(hRead);
-            m_fNoiseTime = g_pLTServer->ReadFromMessageFloat(hRead);
-            m_fLastPainTime = g_pLTServer->ReadFromMessageFloat(hRead);
-            m_fLastPainVolume = g_pLTServer->ReadFromMessageFloat(hRead);
+			m_eCharacterDeath = (CharacterDeath)g_pLTServer->ReadFromMessageDWord(hRead);
+			m_eCharacterClass = (CharacterClass)g_pLTServer->ReadFromMessageDWord(hRead);
+			g_pLTServer->ReadFromLoadSaveMessageObject(hRead, &m_hSceneObject);
+			m_fNoiseVolume = g_pLTServer->ReadFromMessageFloat(hRead);
+			m_fNoiseTime = g_pLTServer->ReadFromMessageFloat(hRead);
+			m_fLastPainTime = g_pLTServer->ReadFromMessageFloat(hRead);
+			m_fLastPainVolume = g_pLTServer->ReadFromMessageFloat(hRead);
 		}
 
 	protected : // Protected methods
@@ -88,13 +86,13 @@ class CDeathScene
 
 		void Set(CCharacter *pCharacter, Body *pBody);
 
-        void SetNoise(LTFLOAT fVolume, LTFLOAT fTime)
+		void SetNoise(LTFLOAT fVolume, LTFLOAT fTime)
 		{
 			m_fNoiseVolume = fVolume;
 			m_fNoiseTime = fTime;
 		}
 
-        void SetPain(LTFLOAT fVolume, LTFLOAT fTime)
+		void SetPain(LTFLOAT fVolume, LTFLOAT fTime)
 		{
 			m_fLastPainTime = fTime;
 			m_fLastPainVolume = fVolume;
@@ -106,15 +104,13 @@ class CDeathScene
 		CharacterClass	 m_eCharacterClass;
 
 		// When a body lingers
-
 		HOBJECT		m_hSceneObject;
 
 		// When there is no body
-
-        LTFLOAT      m_fNoiseVolume;
-        LTFLOAT      m_fNoiseTime;
-        LTFLOAT      m_fLastPainVolume;
-        LTFLOAT      m_fLastPainTime;
+		LTFLOAT	m_fNoiseVolume;
+		LTFLOAT	m_fNoiseTime;
+		LTFLOAT	m_fLastPainVolume;
+		LTFLOAT	m_fLastPainTime;
 };
 
 #include "Body.h"
