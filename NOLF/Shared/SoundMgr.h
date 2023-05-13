bc49cd8218bd5fa7ebbf07f8a86475fd5a3e3a98
BreakinBenny <fox@blaj.net>
2023-05-13 17:32:22 +0200

    Update SoundMgr.h

--- NOLF/Shared/SoundMgr.h
+++ NOLF/Shared/SoundMgr.h
@@ -1,13 +1,11 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : SoundMgr.h
+// MODULE: SoundMgr.h
 //
 // PURPOSE : SoundMgr definition - Controls sound
 //
 // CREATED : 2/08/99
 //
 // (c) 1999-2000 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __SOUND_MGR_H__
@@ -19,35 +17,35 @@
 
 // #defines...
 
-#define SMGR_DEFAULT_VOLUME		100
+#define SMGR_DEFAULT_VOLUME	100
 
 class CGameSoundMgr : public CGameButeMgr
 {
 	public :
 
-        virtual LTBOOL Init(ILTCSBase *pInterface, const char* szAttributeFile);
+	virtual LTBOOL Init(ILTCSBase *pInterface, const char* szAttributeFile);
 
-        HLTSOUND    PlaySoundFromObject(HOBJECT hObject, char *pName, LTFLOAT fRadius,
-            SoundPriority ePriority, uint32 dwFlags=0, uint8 nVolume=SMGR_DEFAULT_VOLUME, float fPitchShift=1.0f);
+	HLTSOUND PlaySoundFromObject(HOBJECT hObject, char *pName, LTFLOAT fRadius,
+			SoundPriority ePriority, uint32 dwFlags=0, uint8 nVolume=SMGR_DEFAULT_VOLUME, float fPitchShift=1.0f);
 
-        HLTSOUND    PlaySoundFromPos(LTVector & vPos, char *pName, LTFLOAT fRadius,
-            SoundPriority ePriority, uint32 dwFlags=0, uint8 nVolume=SMGR_DEFAULT_VOLUME, float fPitchShift=1.0f);
+	HLTSOUND PlaySoundFromPos(LTVector & vPos, char *pName, LTFLOAT fRadius,
+			SoundPriority ePriority, uint32 dwFlags=0, uint8 nVolume=SMGR_DEFAULT_VOLUME, float fPitchShift=1.0f);
 
-		CString		GetRandomSoundFilename(const char* pTag, const char* pAttributeBase);
-		CString		GetSoundFilename(const char* pTag, const char* pAttribute);
-        CString     GetSoundFilenameFromId(const char* pPathAttribute, uint32 dwId);
-		uint32		GetSoundIdFromFilename(const char* pSoundFile);
+		CString	GetRandomSoundFilename(const char* pTag, const char* pAttributeBase);
+		CString	GetSoundFilename(const char* pTag, const char* pAttribute);
+		CString	GetSoundFilenameFromId(const char* pPathAttribute, uint32 dwId);
+		uint32	GetSoundIdFromFilename(const char* pSoundFile);
 
 	protected :
 
-        CGameSoundMgr();
-        virtual ~CGameSoundMgr();
+	CGameSoundMgr();
+	virtual ~CGameSoundMgr();
 
 		virtual	HLTSOUND PlaySound(PlaySoundInfo & playSoundInfo) = 0;
 		virtual	LTVector GetObjectPos(HOBJECT hObj) = 0;
 
 		ILTCSBase*	m_pInterface;
-        LTFLOAT		m_fInnerRadiusPercent;  // Percent of outer radius used for inner radius
+		LTFLOAT		m_fInnerRadiusPercent;	// Percent of outer radius used for inner radius
 };
 
 #endif // __SOUND_MGR_H__
