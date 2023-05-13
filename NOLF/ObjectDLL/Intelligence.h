95c2b887b75d4140a130eb05f3738734625b8277
BreakinBenny <fox@blaj.net>
2023-05-13 16:50:32 +0200

    Update Intelligence.h

--- NOLF/ObjectDLL/Intelligence.h
+++ NOLF/ObjectDLL/Intelligence.h
@@ -1,13 +1,11 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : Intelligence.h
+// MODULE: Intelligence.h
 //
 // PURPOSE : An Intelligence object
 //
 // CREATED : 9/14/99
 //
 // (c) 1999-2000 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __INTELLIGENCE_H__
@@ -25,8 +23,8 @@ class Intelligence : public Prop
 
 	protected :
 
-        virtual uint32  EngineMessageFn(uint32 messageID, void *pData, LTFLOAT lData);
-        virtual uint32  ObjectMessageFn(HOBJECT hSender, uint32 messageID, HMESSAGEREAD hRead);
+		virtual uint32 EngineMessageFn(uint32 messageID, void *pData, LTFLOAT lData);
+		virtual uint32 ObjectMessageFn(HOBJECT hSender, uint32 messageID, HMESSAGEREAD hRead);
 
 		void	ReadProp(ObjectCreateStruct *pData);
 		void	PostPropRead(ObjectCreateStruct* pData);
@@ -39,50 +37,50 @@ class Intelligence : public Prop
 		void	Save(HMESSAGEWRITE hWrite);
 		void	Load(HMESSAGEREAD hRead);
 
-		LTBOOL		m_bShowPopup;
-        LTBOOL		m_bPhotoOnly;
-		HSTRING		m_hstrPickedUpCmd;	// Command to send when intelligence is picked up / photographed
+		LTBOOL	m_bShowPopup;
+		LTBOOL	m_bPhotoOnly;
+		HSTRING	m_hstrPickedUpCmd;	// Command to send when intelligence is picked up / photographed
 
-		uint8		m_nPlayerTeamFilter;
-		uint32		m_nIntelId;
-		uint32		m_nInfoId;
+		uint8	m_nPlayerTeamFilter;
+		uint32	m_nIntelId;
+		uint32	m_nInfoId;
 
-		LTBOOL		m_bStartHidden;
-		LTBOOL		m_bFirstUpdate;
-        LTFLOAT		m_fRespawnDelay;            // How quickly we respawn
-		HSTRING		m_hstrRespawnSoundFile;		// Sound to play when item respawns
+		LTBOOL	m_bStartHidden;
+		LTBOOL	m_bFirstUpdate;
+		LTFLOAT	m_fRespawnDelay;	// How quickly we respawn
+		HSTRING	m_hstrRespawnSoundFile;	// Sound to play when item respawns
 
 
 		LTBOOL		m_bSkipUpdate; 
 
-        void InitialUpdate();
-        void CacheFiles();
-        LTBOOL Update();
+		void InitialUpdate();
+		void CacheFiles();
+		LTBOOL Update();
 
 };
 
 class CIntelPlugin : public CPropPlugin
 {
-  public:
+	public:
 
-    virtual LTRESULT PreHook_EditStringList(
+	virtual LTRESULT PreHook_EditStringList(
 		const char* szRezPath,
 		const char* szPropName,
 		char** aszStrings,
-        uint32* pcStrings,
-        const uint32 cMaxStrings,
-        const uint32 cMaxStringLength);
+		uint32* pcStrings,
+		const uint32 cMaxStrings,
+		const uint32 cMaxStringLength);
 
 	virtual LTRESULT PreHook_Dims(
-			const char* szRezPath,
-			const char* szPropValue,
-			char* szModelFilenameBuf,
-			int	  nModelFilenameBufLen,
+			const	char* szRezPath,
+			const	char* szPropValue,
+			char*	szModelFilenameBuf,
+			int	nModelFilenameBufLen,
 			LTVector & vDims);
 
-  protected :
+	protected :
 
-	  	  CIntelMgrPlugin m_IntelMgrPlugin;
+			CIntelMgrPlugin m_IntelMgrPlugin;
 };
 
 #endif // __INTELLIGENCE_H__
