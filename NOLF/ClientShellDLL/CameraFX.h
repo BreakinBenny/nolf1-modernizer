02134f91ff0bab42137b357b03c22d140609d4c9
BreakinBenny <fox@blaj.net>
2023-05-13 12:19:36 +0200

    Update CameraFX.h

--- NOLF/ClientShellDLL/CameraFX.h
+++ NOLF/ClientShellDLL/CameraFX.h
@@ -1,11 +1,9 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : CameraFX.h
+// MODULE: CameraFX.h
 //
 // PURPOSE : Camera special fx class - Definition
 //
 // CREATED : 5/20/98
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __CAMERA_FX_H__
@@ -15,18 +13,18 @@
 
 struct CAMCREATESTRUCT : public SFXCREATESTRUCT
 {
-    CAMCREATESTRUCT();
+	CAMCREATESTRUCT();
 
-    LTBOOL   bAllowPlayerMovement;
-    uint8    nCameraType;
-    LTBOOL   bIsListener;
+	LTBOOL	bAllowPlayerMovement;
+	uint8	nCameraType;
+	LTBOOL	bIsListener;
 };
 
 inline CAMCREATESTRUCT::CAMCREATESTRUCT()
 {
-    bAllowPlayerMovement    = LTFALSE;
-	nCameraType				= 0;
-    bIsListener             = LTFALSE;
+	bAllowPlayerMovement	= LTFALSE;
+	nCameraType		= 0;
+	bIsListener		= LTFALSE;
 }
 
 class CCameraFX : public CSpecialFX
@@ -35,37 +33,37 @@ class CCameraFX : public CSpecialFX
 
 		CCameraFX() : CSpecialFX()
 		{
-            m_bAllowPlayerMovement  = LTFALSE;
-			m_nCameraType			= 0;
-            m_bIsListener           = LTFALSE;
+			m_bAllowPlayerMovement	= LTFALSE;
+			m_nCameraType		= 0;
+			m_bIsListener		= LTFALSE;
 		}
 
-        virtual LTBOOL Update() { return !m_bWantRemove; }
+	virtual LTBOOL Update() { return !m_bWantRemove; }
 
-        LTBOOL Init(SFXCREATESTRUCT* psfxCreateStruct)
-		{
-            if (!CSpecialFX::Init(psfxCreateStruct)) return LTFALSE;
+	LTBOOL Init(SFXCREATESTRUCT* psfxCreateStruct)
+	{
+		if (!CSpecialFX::Init(psfxCreateStruct)) return LTFALSE;
 
-			CAMCREATESTRUCT* pCAM = (CAMCREATESTRUCT*)psfxCreateStruct;
+		CAMCREATESTRUCT* pCAM = (CAMCREATESTRUCT*)psfxCreateStruct;
 
-			m_bAllowPlayerMovement	= pCAM->bAllowPlayerMovement;
-			m_nCameraType			= pCAM->nCameraType;
-			m_bIsListener			= pCAM->bIsListener;
+		m_bAllowPlayerMovement	= pCAM->bAllowPlayerMovement;
+		m_nCameraType		= pCAM->nCameraType;
+		m_bIsListener		= pCAM->bIsListener;
 
-            return LTTRUE;
-		}
+		return LTTRUE;
+	}
 
-        LTBOOL   AllowPlayerMovement()   const { return m_bAllowPlayerMovement; }
-        uint8    GetType()               const { return m_nCameraType; }
-        LTBOOL   IsListener()            const { return m_bIsListener; }
+	LTBOOL	AllowPlayerMovement()	const { return m_bAllowPlayerMovement; }
+	uint8	GetType()		const { return m_nCameraType; }
+	LTBOOL	IsListener()		const { return m_bIsListener; }
 
 		virtual uint32 GetSFXID() { return SFX_CAMERA_ID; }
 
 	protected :
 
-        LTBOOL   m_bAllowPlayerMovement;
-        uint8    m_nCameraType;
-        LTBOOL   m_bIsListener;
+	LTBOOL	m_bAllowPlayerMovement;
+	uint8	m_nCameraType;
+	LTBOOL	m_bIsListener;
 };
 
 #endif // __CAMERA_FX_H__
