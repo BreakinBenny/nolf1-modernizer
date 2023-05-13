fc3868f654294ef4a7a37d133df95d74a46d6612
BreakinBenny <fox@blaj.net>
2023-05-13 13:38:15 +0200

    Update FolderAudio.h

--- NOLF/ClientShellDLL/FolderAudio.h
+++ NOLF/ClientShellDLL/FolderAudio.h
@@ -18,14 +18,14 @@ public:
 	virtual ~CFolderAudio();
 
 	// Build the folder
-    LTBOOL   Build();
-    void    OnFocus(LTBOOL bFocus);
+	LTBOOL	Build();
+	void	OnFocus(LTBOOL bFocus);
 
 	// Override the left and right controls so that the volumes can be changed
-    LTBOOL   OnLeft();
-    LTBOOL   OnRight();
-    LTBOOL   OnEnter();
-    LTBOOL   OnLButtonUp(int x, int y);
+	LTBOOL OnLeft();
+	LTBOOL OnRight();
+	LTBOOL OnEnter();
+	LTBOOL OnLButtonUp(int x, int y);
 
 protected:
 	void	LoadSoundSettings();
@@ -34,20 +34,20 @@ protected:
 	// Enable/Disable the volume controls based on m_bSoundOn and m_bMusicOn
 	void	EnableDisableControls();
 
-    uint32  OnCommand(uint32 dwCommand, uint32 dwParam1, uint32 dwParam2);
+	uint32	OnCommand(uint32 dwCommand, uint32 dwParam1, uint32 dwParam2);
 
 protected:
-    LTBOOL   m_bSoundOn;
-    LTBOOL   m_bMusicOn;
-    LTBOOL   m_bOldSoundOn;
-    LTBOOL   m_bOldMusicOn;
-	int		m_nSoundVolume;
-	int		m_nMusicVolume;
-    LTBOOL   m_bSoundQuality;
-    LTBOOL   m_bOldSoundQuality;
+	LTBOOL	m_bSoundOn;
+	LTBOOL	m_bMusicOn;
+	LTBOOL	m_bOldSoundOn;
+	LTBOOL	m_bOldMusicOn;
+	int	m_nSoundVolume;
+	int	m_nMusicVolume;
+	LTBOOL	m_bSoundQuality;
+	LTBOOL	m_bOldSoundQuality;
 
-	CSliderCtrl		*m_pSoundVolumeCtrl;
-	CSliderCtrl		*m_pMusicVolumeCtrl;
+	CSliderCtrl	*m_pSoundVolumeCtrl;
+	CSliderCtrl	*m_pMusicVolumeCtrl;
 	CToggleCtrl	*m_pSoundQualityCtrl;
 
 };
