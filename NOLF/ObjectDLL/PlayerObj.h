cf8e0faf63d23c97dcb049cc79ca310e96364393
BreakinBenny <fox@blaj.net>
2023-05-13 17:08:58 +0200

    Update PlayerObj.h

--- NOLF/ObjectDLL/PlayerObj.h
+++ NOLF/ObjectDLL/PlayerObj.h
@@ -1,5 +1,4 @@
 // ----------------------------------------------------------------------- //
-//
 // MODULE  : PlayerObj.h
 //
 // PURPOSE : Player object definition
@@ -7,7 +6,6 @@
 // CREATED : 9/18/97
 //
 // (c) 1997-2001 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __CPLAYER_OBJ_H__
@@ -24,8 +22,8 @@
 #include "AnimatorPlayer.h"
 #include "PlayerShared.h"
 
-#define DEFAULT_PLAYERNAME		"Player"
-#define NET_NAME_LENGTH			MAX_PLAYER_NAME
+#define DEFAULT_PLAYERNAME	"Player"
+#define NET_NAME_LENGTH		MAX_PLAYER_NAME
 
 class CPlayerAttachments;
 class CWeapon;
@@ -35,8 +33,8 @@ class TeleportPoint;
 
 struct FlashlightInfo
 {
-    LTBOOL           bOn;
-    LTVector         vPos;
+	LTBOOL		bOn;
+	LTVector	vPos;
 
 	FlashlightInfo()
 	{
@@ -45,7 +43,7 @@ struct FlashlightInfo
 
 	void Clear()
 	{
-        bOn = LTFALSE;
+		bOn = LTFALSE;
 	}
 
 	void Save(HMESSAGEWRITE hWrite)
@@ -81,12 +79,12 @@ class CPlayerObj : public CCharacter
 
 		// If the update had CLIENTUPDATE_ALLOWINPUT, then it returns FALSE because nothing
 		// else will be in the packet.
-        LTBOOL ClientUpdate(HMESSAGEREAD hMessage);
+		LTBOOL ClientUpdate(HMESSAGEREAD hMessage);
 
-        LTBOOL MultiplayerInit(HMESSAGEREAD hMessage);
+		LTBOOL MultiplayerInit(HMESSAGEREAD hMessage);
 
-        void Respawn(uint8 nServerLoadGameFlags=LOAD_NEW_LEVEL);
-        void SetSpectatorMode(LTBOOL bOn);
+		void Respawn(uint8 nServerLoadGameFlags=LOAD_NEW_LEVEL);
+		void SetSpectatorMode(LTBOOL bOn);
 		void ToggleRunLock();
 		void ToggleGodMode();
 
@@ -103,10 +101,10 @@ class CPlayerObj : public CCharacter
 
 		void RideVehicle(PlayerVehicle* pVehicle);
 
-        LTBOOL HasDangerousWeapon();
+		LTBOOL HasDangerousWeapon();
 
-        void ChangeWeapon(uint8 nCommandId, LTBOOL bAuto=LTFALSE, int32 nAmmoId=-1);
-        void SetRunLock(LTBOOL bRunLock)     { m_bRunLock = bRunLock; }
+		void ChangeWeapon(uint8 nCommandId, LTBOOL bAuto=LTFALSE, int32 nAmmoId=-1);
+		void SetRunLock(LTBOOL bRunLock)	{ m_bRunLock = bRunLock; }
 		void ChangeState(PlayerState eNewState);
 		PlayerState GetState() const { return m_eState; }
 
@@ -117,24 +115,24 @@ class CPlayerObj : public CCharacter
 		void HandleWeaponFireMessage(HMESSAGEREAD hRead);
 		void HandleWeaponSoundMessage(HMESSAGEREAD hRead);
 		void HandleClientMsg(HMESSAGEREAD hRead);
-        LTBOOL HandleObjectiveMessage(char* pMsg);
-        LTBOOL HandleTransmissionMessage(char* pMsg);
-        LTBOOL HandleCreditsMessage(char* pMsg);
-        LTBOOL HandlePopupMessage(char* pMsg);
-        LTBOOL HandleFadeScreenMessage(char* pMsg, LTBOOL bFadeIn);
-        LTBOOL HandleMissionTextMessage(char* pMsg);
-        LTBOOL HandleMissionFailedMessage(char* pMsg);
-        LTBOOL HandleMusicMessage(const char* szMsg);
+		LTBOOL HandleObjectiveMessage(char* pMsg);
+		LTBOOL HandleTransmissionMessage(char* pMsg);
+		LTBOOL HandleCreditsMessage(char* pMsg);
+		LTBOOL HandlePopupMessage(char* pMsg);
+		LTBOOL HandleFadeScreenMessage(char* pMsg, LTBOOL bFadeIn);
+		LTBOOL HandleMissionTextMessage(char* pMsg);
+		LTBOOL HandleMissionFailedMessage(char* pMsg);
+		LTBOOL HandleMusicMessage(const char* szMsg);
 
 		void	SetNetName(const char* sNetName); 
 		char*	GetNetName() { return(m_sNetName); }
-        uint32  GetTeamID() { return(m_dwTeamID); }
+		uint32	GetTeamID() { return(m_dwTeamID); }
 		void	UpdateTeamID();
 
 		void	SetChatting(LTBOOL bChatting);
 
 		//used only for ending co-op levels
-		LTBOOL  IsReadyToExit() {return m_bReadyToExit;}
+		LTBOOL	IsReadyToExit() {return m_bReadyToExit;}
 		void	ReadyToExit(LTBOOL bReady) {m_bReadyToExit = bReady;}
 
 		void	ForceDuck(LTBOOL bForce) {m_bForceDuck = bForce;}
@@ -142,21 +140,21 @@ class CPlayerObj : public CCharacter
 		// called after the player has been created, but before it is initted for multiplayer
 		void	PreMultiplayerInit();
 
-		int   GetFragCount() { return(m_nFragCount); }
-		void  SetFragCount(int nFrags);
-		void  IncFragCount();
-		void  DecFragCount();
+		int	GetFragCount() { return(m_nFragCount); }
+		void	SetFragCount(int nFrags);
+		void	IncFragCount();
+		void	DecFragCount();
 
-		void  AddToScore(int nAdd);
+		void	AddToScore(int nAdd);
 
-		int   GetRespawnCount() { return(m_nRespawnCount); }
-		void  SetRespawnCount(int nRespawns) { m_nRespawnCount = nRespawns; }
-		void  IncRespawnCount() { m_nRespawnCount++; }
+		int	GetRespawnCount() { return(m_nRespawnCount); }
+		void	SetRespawnCount(int nRespawns) { m_nRespawnCount = nRespawns; }
+		void	IncRespawnCount() { m_nRespawnCount++; }
 
-        LTBOOL IsFlashlightOn() const { return m_FlashlightInfo.bOn; }
-        const LTVector& GetFlashlightPos() const { return m_FlashlightInfo.vPos; }
+		LTBOOL IsFlashlightOn() const { return m_FlashlightInfo.bOn; }
+		const LTVector& GetFlashlightPos() const { return m_FlashlightInfo.vPos; }
 
-        void    DoWeaponChange(uint8 nWeaponId);
+		void	DoWeaponChange(uint8 nWeaponId);
 
 		void SetRestoreAmmoId(LTFLOAT fRestoreAmmoId) { m_fRestoreAmmoId = fRestoreAmmoId; }
 
@@ -164,7 +162,7 @@ class CPlayerObj : public CCharacter
 
 		void TeleportClientToServerPos();
 
-        LTFLOAT ComputeDamageModifier(ModelNode eModelNode);
+		LTFLOAT ComputeDamageModifier(ModelNode eModelNode);
 
 		const char* GetHeadSkinFilename() const;
 
@@ -175,14 +173,14 @@ class CPlayerObj : public CCharacter
 		void FinishHandshake() { m_bHasDoneHandshake = LTTRUE; }
 	protected :
 
-        uint32 EngineMessageFn(uint32 messageID, void *pData, LTFLOAT lData);
-        uint32 ObjectMessageFn(HOBJECT hSender, uint32 messageID, HMESSAGEREAD hRead);
+		uint32 EngineMessageFn(uint32 messageID, void *pData, LTFLOAT lData);
+		uint32 ObjectMessageFn(HOBJECT hSender, uint32 messageID, HMESSAGEREAD hRead);
 
-        void    WeaponCheat(uint8 nWeaponId);
+		void	WeaponCheat(uint8 nWeaponId);
 
 		void	Reset();
 		void	StartDeath();
-        void    HandleDead(LTBOOL bRemoveObj);
+		void	HandleDead(LTBOOL bRemoveObj);
 		void	InitializeWeapons(CWeapons* pWeapons);
 		char*	GetDamageSound(DamageType eType);
 		char*	GetDeathSound();
@@ -191,10 +189,10 @@ class CPlayerObj : public CCharacter
 
 		virtual void ProcessDamageMsg(HMESSAGEREAD hRead);
 
-        virtual LTVector GetHeadOffset() { return GetPlayerHeadOffset(g_pModelButeMgr, m_eModelStyle); }
-        virtual LTBOOL ProcessCommand(char** pTokens, int nArgs, char* pNextCommand);
+		virtual LTVector GetHeadOffset() { return GetPlayerHeadOffset(g_pModelButeMgr, m_eModelStyle); }
+		virtual LTBOOL ProcessCommand(char** pTokens, int nArgs, char* pNextCommand);
 
-        virtual LTBOOL Activate(LTVector vPos, LTVector vDir, LTBOOL bEditMode=LTFALSE);
+		virtual LTBOOL Activate(LTVector vPos, LTVector vDir, LTBOOL bEditMode=LTFALSE);
 
 		virtual void PreCreateSpecialFX(CHARCREATESTRUCT& cs);
 
@@ -202,21 +200,21 @@ class CPlayerObj : public CCharacter
 	private :
 
 		void	PostPropRead(ObjectCreateStruct *pStruct);
-        LTBOOL   InitialUpdate(int nInfo);
-        LTBOOL   Update();
+		LTBOOL	InitialUpdate(int nInfo);
+		LTBOOL	Update();
 
 		void	CreatePVWeapon();
 
-		virtual void UpdateCommands();		// Update our animator based on client commands
-		virtual void UpdateMovement();		// Update player movement
+		virtual void UpdateCommands();	// Update our animator based on client commands
+		virtual void UpdateMovement();	// Update player movement
 
-        void UpdateInContainerState(HOBJECT* objContainers, uint32 & nContainers);
+		void UpdateInContainerState(HOBJECT* objContainers, uint32 & nContainers);
 
-		void UpdateClientPhysics();		// Update client physics state.
-		void UpdateAirLevel();			// Update player's air
-		void UpdateClientEffects();		// Update client side effects
+		void UpdateClientPhysics();	// Update client physics state.
+		void UpdateAirLevel();		// Update player's air
+		void UpdateClientEffects();	// Update client side effects
 
-        void UpdateInterface(LTBOOL bForceUpdate=LTFALSE); // Send client interface info.
+		void UpdateInterface(LTBOOL bForceUpdate=LTFALSE); // Send client interface info.
 		void ResetPlayer(LTBOOL bSpectatorChange=LTFALSE); // Reset the player
 		void ResetHealth();
 		void ResetInventory(LTBOOL bRemoveGear=LTTRUE);
@@ -224,16 +222,16 @@ class CPlayerObj : public CCharacter
 
 		void TweakCameraOffset();
 
-        void Save(HMESSAGEWRITE hWrite, uint32 dwSaveFlags);
-        void Load(HMESSAGEREAD hRead, uint32 dwLoadFlags);
+		void Save(HMESSAGEWRITE hWrite, uint32 dwSaveFlags);
+		void Load(HMESSAGEREAD hRead, uint32 dwLoadFlags);
 
 		void DoAutoSave();
 
-        LTBOOL ProcessTriggerMsg(HMESSAGEREAD hRead);
+		LTBOOL ProcessTriggerMsg(HMESSAGEREAD hRead);
 
-        LTBOOL PlayingCinematic(LTBOOL bStopCinematic=LTFALSE);
+		LTBOOL PlayingCinematic(LTBOOL bStopCinematic=LTFALSE);
 
-		void  HandleGameRestore();
+		void	HandleGameRestore();
 
 		void SetForceUpdateList(ForceUpdate* pFU);
 		void UpdateSpecialFX();
@@ -241,7 +239,7 @@ class CPlayerObj : public CCharacter
 		void UpdateClientViewPos();
 		void UpdateConsoleVars();
 
-        void TeleFragObjects(LTVector & vPos);
+		void TeleFragObjects(LTVector & vPos);
 
 		void HandleTeleport(TeleportPoint* pTeleportPoint);
 
@@ -284,26 +282,25 @@ class CPlayerObj : public CCharacter
 		void	ChangeToWeapon(char* pWeaponName);
 
 
-	private :  // Data members
+	private :	// Data members
 
-        LTFLOAT      m_fOldHitPts;
-        LTFLOAT      m_fOldArmor;
-        LTFLOAT      m_fOldAirLevel;
-        LTFLOAT      m_fAirLevel;
-		LTFLOAT		 m_fDamageTime;
-        LTVector     m_vOldModelColor;
-        LTFLOAT      m_fOldModelAlpha;
+		LTFLOAT		m_fOldHitPts;
+		LTFLOAT		m_fOldArmor;
+		LTFLOAT		m_fOldAirLevel;
+		LTFLOAT		m_fAirLevel;
+		LTFLOAT		m_fDamageTime;
+		LTVector	m_vOldModelColor;
+		LTFLOAT		m_fOldModelAlpha;
 
 		// When we respawn, this is incremented so we ignore packets from an older teleport.
-
-        uint8       m_ClientMoveCode;
+		uint8	m_ClientMoveCode;
 
 		PlayerState m_eState;
 
-        LTBOOL      m_b3rdPersonView;
-        uint32      m_nSavedFlags;
-		GameType	m_eGameType;
-		LTBOOL		m_bRespawnCalled;
+		LTBOOL	m_b3rdPersonView;
+		uint32	m_nSavedFlags;
+		GameType m_eGameType;
+		LTBOOL	m_bRespawnCalled;
 
 		// Animator
 
@@ -311,26 +308,26 @@ class CPlayerObj : public CCharacter
 
 		// Info about cheats...
 
-        LTBOOL       m_bRunLock;
-        LTBOOL       m_bTweakingMovement;
-        LTBOOL       m_bGodMode;
+		LTBOOL	m_bRunLock;
+		LTBOOL	m_bTweakingMovement;
+		LTBOOL	m_bGodMode;
 
-        LTBOOL       m_bAllowInput;
+		LTBOOL	m_bAllowInput;
 
-        LTBOOL       m_bChatting;
+		LTBOOL	m_bChatting;
 
-	    uint16      m_nClientChangeFlags;
+		uint16	m_nClientChangeFlags;
 
-		HMESSAGEREAD	m_hClientSaveData;  // Client data to save
+		HMESSAGEREAD	m_hClientSaveData;	// Client data to save
 
-		HSTRING			m_hstrStartLevelTriggerTarget;
-		HSTRING			m_hstrStartLevelTriggerMessage;
+		HSTRING		m_hstrStartLevelTriggerTarget;
+		HSTRING		m_hstrStartLevelTriggerMessage;
 
 		PlayerPhysicsModel	m_ePPhysicsModel;
-		HOBJECT				m_hVehicleModel;
+		HOBJECT			m_hVehicleModel;
 
-		LTBOOL				m_bReadyToExit;
-		LTBOOL				m_bForceDuck;
+		LTBOOL			m_bReadyToExit;
+		LTBOOL			m_bForceDuck;
 
 		CPlayerSummaryMgr	m_PlayerSummary;
 
@@ -339,61 +336,59 @@ class CPlayerObj : public CCharacter
 	// should be added here (to keep them together)...
 
 
-		CPlayerAttachments*		m_pPlayerAttachments;	// Our attachments aggregate
+		CPlayerAttachments*	m_pPlayerAttachments;	// Our attachments aggregate
 
-		HOBJECT		m_CurContainers[MAX_TRACKED_CONTAINERS];
-        uint32      m_nCurContainers;
+		HOBJECT	m_CurContainers[MAX_TRACKED_CONTAINERS];
+		uint32	m_nCurContainers;
 
-        LTBOOL      m_bFirstUpdate;
-		LTBOOL		m_bNewLevel;
+		LTBOOL	m_bFirstUpdate;
+		LTBOOL	m_bNewLevel;
 
-        uint32      m_PStateChangeFlags;        // Which things need to get sent to client.
+		uint32	m_PStateChangeFlags;	// Which things need to get sent to client.
 
-		int*			m_pnOldAmmo;
+		int*	m_pnOldAmmo;
 
-		HCLIENT			m_hClient;
+		HCLIENT	m_hClient;
 
 		// Data members used in load/save...
-
-        uint32      m_dwLastLoadFlags;
+		uint32	m_dwLastLoadFlags;
 
 
 		// Multiplayer data
 
-		char		m_sNetName[NET_NAME_LENGTH];
-		int			m_nFragCount;
-		int			m_nRespawnCount;
-        uint32      m_dwTeamID;
+		char	m_sNetName[NET_NAME_LENGTH];
+		int	m_nFragCount;
+		int	m_nRespawnCount;
+		uint32	m_dwTeamID;
 
-		LTBOOL		m_bHasDoneHandshake; // Is the handshaking done yet?
+		LTBOOL	m_bHasDoneHandshake;	// Is the handshaking done yet?
 
 		// ForceUpdate camera support...
 
 		CTList<LPBASECLASS>	m_Cameras;
-        LTBOOL               m_bCameraListBuilt;
+		LTBOOL			m_bCameraListBuilt;
 
 
 		// Motion status from client
-
-        uint8       m_nMotionStatus;
-        uint8       m_nWeaponStatus;
+		uint8	m_nMotionStatus;
+		uint8	m_nWeaponStatus;
 
 		// BL 09/30/00 m_fRestorAmmoId is a HACK to fix ammo save/load 
-		LTFLOAT		m_fRestoreAmmoId;
+		LTFLOAT	m_fRestoreAmmoId;
 
 
 		// Flashlight info
 
-		FlashlightInfo		m_FlashlightInfo;
+		FlashlightInfo	m_FlashlightInfo;
 
 		// Client/server movement interaction variables
 		LTVector	m_vLastClientPos;	// Last position provided by the client
-		LTBOOL		m_bUseLeash;	    // Is leashing turned on?
+		LTBOOL		m_bUseLeash;		// Is leashing turned on?
 
 		// Added for Update 1.002.  This is used to store the player's ziphook
 		// state...
 
-		uint8		m_nZipState;
+		uint8	m_nZipState;
 
 		// Console variables used to tweak player movement...
 
@@ -406,18 +401,17 @@ class CPlayerObj : public CCharacter
 		CVarTrack	m_LadderVelTrack;
 		CVarTrack	m_ZipCordVelTrack;
 
-		float		m_fLeashLen;
-		float		m_fLeashSpring;
-		float		m_fLeashSpringRate;
-		float		m_fZipCordVel;
+		float	m_fLeashLen;
+		float	m_fLeashSpring;
+		float	m_fLeashSpringRate;
+		float	m_fZipCordVel;
 
-        LTBOOL       m_bShowNodes;
-        LTBOOL       m_bLevelStarted;
-        LTBOOL       m_bWaitingForAutoSave;
+		LTBOOL	m_bShowNodes;
+		LTBOOL	m_bLevelStarted;
+		LTBOOL	m_bWaitingForAutoSave;
 
-		char		m_szMultiplayerSkin[128];
-		char		m_szMultiplayerHead[128];
+		char	m_szMultiplayerSkin[128];
+		char	m_szMultiplayerHead[128];
 };
-
 
 #endif  // __CPLAYER_OBJ_H__
