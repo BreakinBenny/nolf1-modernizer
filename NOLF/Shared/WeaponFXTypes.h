b0da541c7e27f9e0e6850105660a3e3daff95a4e
BreakinBenny <fox@blaj.net>
2023-05-14 01:55:10 +0200

    Update WeaponFXTypes.h

--- NOLF/Shared/WeaponFXTypes.h
+++ NOLF/Shared/WeaponFXTypes.h
@@ -1,14 +1,12 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : WeaponFXTypes.h
+// MODULE: WeaponFXTypes.h
 //
 // PURPOSE : Weapon FX types contains the different types and enums associated
-//			 with the weapon fx class.
+//		with the weapon fx class.
 //
 // CREATED : 2/22/98
 //
 // (c) 1997-1999 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __WEAPON_FX_TYPES_H__
@@ -16,15 +14,15 @@
 
 // Weapon fx (impact and fire)...
 
-#define	WFX_MARK		(1<<0)
+#define	WFX_MARK	(1<<0)
 
 #define WFX_TINTSCREEN	(1<<2)
 #define WFX_BLASTMARK	(1<<3)
 
-#define	WFX_TRACER		(1<<4)
-#define WFX_MUZZLE		(1<<5)
-#define WFX_SHELL		(1<<6)
-#define WFX_LIGHT		(1<<7)
+#define	WFX_TRACER	(1<<4)
+#define WFX_MUZZLE	(1<<5)
+#define WFX_SHELL	(1<<6)
+#define WFX_LIGHT	(1<<7)
 #define WFX_FIRESOUND	(1<<8)
 #define WFX_EXITMARK	(1<<9)
 #define WFX_EXITDEBRIS	(1<<10)
@@ -36,19 +34,18 @@
 #define WFX_EXITSURFACE (WFX_EXITMARK | WFX_EXITDEBRIS)
 
 // Projectile FX...
-
 #define	PFX_SMOKETRAIL	(1<<0)
-#define	PFX_FLARE		(1<<1)
-#define PFX_LIGHT		(1<<2)
+#define	PFX_FLARE	(1<<1)
+#define PFX_LIGHT	(1<<2)
 #define PFX_FLYSOUND	(1<<3)
 
 // Particle trail types...
 // Bits 0-6 types
 
-#define PT_BLOOD		(1<<0)
-#define PT_SMOKE		(1<<1)
+#define PT_BLOOD	(1<<0)
+#define PT_SMOKE	(1<<1)
 #define PT_SMOKE_LONG	(1<<2)
 #define PT_SMOKE_BLACK	(1<<3)
-#define PT_GIBSMOKE		(1<<4)
+#define PT_GIBSMOKE	(1<<4)
 
 #endif // __WEAPON_FX_TYPES_H__
