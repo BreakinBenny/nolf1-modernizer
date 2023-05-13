291d821ffbac1ddcf82a2275526a17399047f29d
BreakinBenny <fox@blaj.net>
2023-05-13 15:52:01 +0200

    Update PlayerSoundFX.h

--- NOLF/ClientShellDLL/PlayerSoundFX.h
+++ NOLF/ClientShellDLL/PlayerSoundFX.h
@@ -1,13 +1,11 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : PlayerSoundFX.h
+// MODULE: PlayerSoundFX.h
 //
 // PURPOSE : Player Sound fx - Definition
 //
 // CREATED : 10/28/98 (was WeaponSoundFX)
 //
 // (c) 1998-2000 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __PLAYER_SOUND_FX_H__
@@ -17,12 +15,12 @@
 
 struct PLAYERSOUNDCREATESTRUCT : public SFXCREATESTRUCT
 {
-    PLAYERSOUNDCREATESTRUCT();
+	PLAYERSOUNDCREATESTRUCT();
 
-    LTVector    vPos;
-    uint8       nClientId;
-    uint8       nType;
-    uint8       nWeaponId;
+	LTVector vPos;
+	uint8	nClientId;
+	uint8	nType;
+	uint8	nWeaponId;
 };
 
 inline PLAYERSOUNDCREATESTRUCT::PLAYERSOUNDCREATESTRUCT()
@@ -49,18 +47,18 @@ class CPlayerSoundFX : public CSpecialFX
 		{
 		}
 
-        virtual LTBOOL Init(SFXCREATESTRUCT* psfxCreateStruct);
-        virtual LTBOOL CreateObject(ILTClient* pClientDE);
-        virtual LTBOOL Update() { return LTFALSE; }
+	virtual LTBOOL Init(SFXCREATESTRUCT* psfxCreateStruct);
+	virtual LTBOOL CreateObject(ILTClient* pClientDE);
+	virtual LTBOOL Update() { return LTFALSE; }
 
-		virtual uint32 GetSFXID() { return SFX_PLAYERSOUND_ID; }
+	virtual uint32 GetSFXID() { return SFX_PLAYERSOUND_ID; }
 
 	private :
 
-        LTVector	m_vPos;
-        uint8		m_nClientId;
-        uint8		m_nType;
-        uint8		m_nWeaponId;
+	LTVector m_vPos;
+	uint8	m_nClientId;
+	uint8	m_nType;
+	uint8	m_nWeaponId;
 };
 
 #endif // __PLAYER_SOUND_FX_H__
