24c8c95628bf46842b784e062221c5c28a82f1cb
BreakinBenny <fox@blaj.net>
2023-05-14 01:53:42 +0200

    Update SurfaceDefs.h

--- NOLF/Shared/SurfaceDefs.h
+++ NOLF/Shared/SurfaceDefs.h
@@ -1,13 +1,11 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : SurfaceDefs.h
+// MODULE: SurfaceDefs.h
 //
 // PURPOSE : Definition of surface related stuff
 //
 // CREATED : 12/09/99
 //
 // (c) 1999-2000 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __SURFACE_DEFS_H__
@@ -17,13 +15,13 @@
 
 enum SurfaceType 
 {
-	ST_UNKNOWN				= 0,	// Unknown value
-	ST_AIR					= 1,	// Not a surface, but not the sky either
-	ST_FLESH				= 2,	// Human flesh
-	ST_SKY					= 110,	// Sky poly
-	ST_LADDER				= 200,	// Ladder volume brush
-	ST_LIQUID				= 201,	// Liquid volume brush
-	ST_INVISIBLE			= 202	// Invisible surface
+	ST_UNKNOWN	= 0,	// Unknown value
+	ST_AIR		= 1,	// Not a surface, but not the sky either
+	ST_FLESH	= 2,	// Human flesh
+	ST_SKY		= 110,	// Sky poly
+	ST_LADDER	= 200,	// Ladder volume brush
+	ST_LIQUID	= 201,	// Liquid volume brush
+	ST_INVISIBLE	= 202	// Invisible surface
 };
 
 #endif // __SURFACE_DEFS_H__
