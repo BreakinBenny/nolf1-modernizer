d8ee8669085ff35d5e830d2d9cc588c590276f31
BreakinBenny <fox@blaj.net>
2023-05-16 22:49:39 +0200

    Update BulletTrailFX.h

--- NOLF/ClientShellDLL/BulletTrailFX.h
+++ NOLF/ClientShellDLL/BulletTrailFX.h
@@ -1,11 +1,9 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : BulletTrailFX.h
+// MODULE: BulletTrailFX.h
 //
 // PURPOSE : SmokeTrail segment special fx class - Definition
 //
 // CREATED : 3/1/98
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __BULLET_TRAIL_FX_H__
@@ -16,17 +14,17 @@
 
 struct BTCREATESTRUCT : public BPSCREATESTRUCT
 {
-    BTCREATESTRUCT();
+	BTCREATESTRUCT();
 
-    LTVector vStartPos;
-    LTVector vDir;
-    LTVector vColor1;
-    LTVector vColor2;
-    LTFLOAT  fLifeTime;
-    LTFLOAT  fFadeTime;
-    LTFLOAT  fRadius;
-    LTFLOAT  fGravity;
-    LTFLOAT  fNumParticles;
+	LTVector vStartPos;
+	LTVector vDir;
+	LTVector vColor1;
+	LTVector vColor2;
+	LTFLOAT	fLifeTime;
+	LTFLOAT	fFadeTime;
+	LTFLOAT	fRadius;
+	LTFLOAT	fGravity;
+	LTFLOAT	fNumParticles;
 };
 
 inline BTCREATESTRUCT::BTCREATESTRUCT()
@@ -35,10 +33,10 @@ inline BTCREATESTRUCT::BTCREATESTRUCT()
 	vDir.Init();
 	vColor1.Init();
 	vColor2.Init();
-	fLifeTime		= 0.0f;
-	fFadeTime		= 0.0f;
-	fRadius			= 0.0f;
-	fGravity		= 0.0f;
+	fLifeTime	= 0.0f;
+	fFadeTime	= 0.0f;
+	fRadius		= 0.0f;
+	fGravity	= 0.0f;
 	fNumParticles	= 0.0f;
 }
 
@@ -52,38 +50,38 @@ class CBulletTrailFX : public CBaseParticleSystemFX
 			VEC_INIT(m_vLastPos);
 			VEC_INIT(m_vDir);
 
-			m_fLifeTime		= 0.0f;
-			m_fFadeTime		= 0.0f;
+			m_fLifeTime	= 0.0f;
+			m_fFadeTime	= 0.0f;
 			m_fNumParticles	= 100.0f;
 
-            m_bFirstUpdate  = LTTRUE;
-			m_fDistance		= 0.0f;
+			m_bFirstUpdate	= LTTRUE;
+			m_fDistance	= 0.0f;
 			m_fDistTraveled	= 0.0f;
-			m_fTrailVel		= 0.0f;
+			m_fTrailVel	= 0.0f;
 		}
 
-        virtual LTBOOL Init(SFXCREATESTRUCT* psfxCreateStruct);
-        virtual LTBOOL Update();
+		virtual LTBOOL Init(SFXCREATESTRUCT* psfxCreateStruct);
+		virtual LTBOOL Update();
 
 		virtual uint32 GetSFXID() { return SFX_BULLETTRAIL_ID; }
 
 	private :
 
-        LTVector m_vLastPos;     // Last bubble particle position
-        LTVector m_vStartPos;    // Starting position of trail
-        LTVector m_vDir;         // Direction of trail
+	LTVector m_vLastPos;	// Last bubble particle position
+	LTVector m_vStartPos;	// Starting position of trail
+	LTVector m_vDir;	// Direction of trail
 
-        LTFLOAT  m_fFadeTime;    // When system should start to fade
-        LTFLOAT  m_fLifeTime;    // How long system stays around
-        LTFLOAT  m_fNumParticles;// Total number of particles in system
+	LTFLOAT	m_fFadeTime;	// When system should start to fade
+	LTFLOAT	m_fLifeTime;	// How long system stays around
+	LTFLOAT	m_fNumParticles;	// Total number of particles in system
 
-        LTBOOL   m_bFirstUpdate; // First update
-        LTFLOAT  m_fDistance;    // Length of trail
-        LTFLOAT  m_fDistTraveled;// How far have we gone?
-        LTFLOAT  m_fTrailVel;    // Speed of trail
+	LTBOOL	m_bFirstUpdate; // First update
+	LTFLOAT	m_fDistance;	// Length of trail
+	LTFLOAT	m_fDistTraveled;// How far have we gone?
+	LTFLOAT	m_fTrailVel;	// Speed of trail
 
-        LTFLOAT  m_fStartTime;   // When did we start
-        LTFLOAT  m_fLastTime;    // When was the last update
+	LTFLOAT	m_fStartTime;	// When did we start
+	LTFLOAT	m_fLastTime;	// When was the last update
 };
 
 #endif // __BULLET_TRAIL_FX_H__
