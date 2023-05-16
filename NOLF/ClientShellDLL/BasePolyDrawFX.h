779858440176e70e71ef997e922c8bbeb3e1fd38
BreakinBenny <fox@blaj.net>
2023-05-16 22:13:24 +0200

    Update BasePolyDrawFX.h

--- NOLF/ClientShellDLL/BasePolyDrawFX.h
+++ NOLF/ClientShellDLL/BasePolyDrawFX.h
@@ -1,13 +1,11 @@
 // ----------------------------------------------------------------------- //
-//
-// MODULE  : BasePolyDrawFX.h
+// MODULE: BasePolyDrawFX.h
 //
 // PURPOSE : BasePolyDraw special fx class - Definition
 //
 // CREATED : 4/15/99
 //
 // (c) 1999 Monolith Productions, Inc.  All Rights Reserved
-//
 // ----------------------------------------------------------------------- //
 
 #ifndef __BASE_POLY_DRAW_FX_H__
@@ -25,8 +23,8 @@ class CBasePolyDrawFX : public CSpecialFX
 {
 	public :
 
-        virtual LTBOOL CreateObject(ILTClient* pClientDE);
-        void DrawAll(ILTCustomDraw *pDraw);
+	virtual LTBOOL CreateObject(ILTClient* pClientDE);
+	void DrawAll(ILTCustomDraw *pDraw);
 
 		~CBasePolyDrawFX()
 		{
@@ -36,23 +34,23 @@ class CBasePolyDrawFX : public CSpecialFX
 			{
 				if (s_hCanvasObj)
 				{
-                    g_pLTClient->DeleteObject(s_hCanvasObj);
-                    s_hCanvasObj = LTNULL;
+					g_pLTClient->DeleteObject(s_hCanvasObj);
+					s_hCanvasObj = LTNULL;
 				}
 			}
 		}
 
-        void SetPos(LTVector vPos) { m_vPos = vPos; }
-        LTVector GetPos()    const { return m_vPos; }
+		void SetPos(LTVector vPos) { m_vPos = vPos; }
+		LTVector GetPos()    const { return m_vPos; }
 
-        void SetRot(LTRotation rRot) { m_rRot = rRot; }
-        LTRotation GetRot()  const { return m_rRot; }
+		void SetRot(LTRotation rRot) { m_rRot = rRot; }
+		LTRotation GetRot()  const { return m_rRot; }
 
-        void SetFlags(uint32 dwFlags) { m_Flags = dwFlags; }
-        uint32 GetFlags()   const { return m_Flags; }
+		void SetFlags(uint32 dwFlags) { m_Flags = dwFlags; }
+		uint32 GetFlags()   const { return m_Flags; }
 
-        void SetFlags2(uint32 dwFlags) { m_Flags2 = dwFlags; }
-        uint32 GetFlags2()  const { return m_Flags2; }
+		void SetFlags2(uint32 dwFlags) { m_Flags2 = dwFlags; }
+		uint32 GetFlags2()  const { return m_Flags2; }
 
 		static HOBJECT GetGlobalCanvaseObj() { return s_hCanvasObj; }
 
@@ -67,26 +65,26 @@ class CBasePolyDrawFX : public CSpecialFX
 
 			if (!s_bListInited)
 			{
-                s_bListInited = LTTRUE;
-                s_PolyDrawFXList.Init(LTFALSE);
+				s_bListInited = LTTRUE;
+				s_PolyDrawFXList.Init(LTFALSE);
 			}
 
 			s_PolyDrawFXList.Add(this);
 		}
 
-        virtual LTBOOL Draw(ILTCustomDraw *pDraw)=0;
+		virtual LTBOOL Draw(ILTCustomDraw *pDraw)=0;
 
 		// We don't want to have the overhead of an engine object, so we'll just
 		// keep our own copies of HOBJECT data we want to use...
 
-        LTVector         m_vPos;
-        LTRotation       m_rRot;
-        uint32          m_Flags;
-        uint32          m_Flags2;
+		LTVector	m_vPos;
+		LTRotation	m_rRot;
+		uint32		m_Flags;
+		uint32		m_Flags2;
 
-        static LTBOOL            s_bListInited;
+		static LTBOOL		s_bListInited;
 		static PolyDrawFXList	s_PolyDrawFXList;
-		static HOBJECT			s_hCanvasObj;
+		static HOBJECT		s_hCanvasObj;
 };
 
 #endif // __BASE_POLY_DRAW_FX_H__
