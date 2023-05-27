bfbc6f68883609877e27cbf5074b6fa6bf680488
BreakinBenny <fox@blaj.net>
2023-05-27 20:56:45 +0200

    Update GameSpyPlayer.h

--- LT2/lithshared/gamespy/gamespyclientmgr/GameSpyPlayer.h
+++ LT2/lithshared/gamespy/gamespyclientmgr/GameSpyPlayer.h
@@ -1,20 +1,16 @@
 /****************************************************************************
+;	 MODULE: GAMESPYPLAYER (.H)
 ;
-;	 MODULE:		GAMESPYPLAYER (.H)
+;	PURPOSE: Game Spy Player Encapsulation Class
 ;
-;	PURPOSE:		Game Spy Player Encapsulation Class
+;	HISTORY: 08/01/00  [blg]  This file was created
 ;
-;	HISTORY:		08/01/00  [blg]  This file was created
-;
-;	COMMENT:		Copyright (c) 2000, LithTech, Inc.
-;
+;	COMMENT: Copyright (c) 2000, LithTech, Inc.
 ****************************************************************************/
 
 #pragma once
 
-
 // Includes...
-
 #ifdef GSCM_LOCAL_BUILD
 #include "..\cengine\goaceng.h"
 #else
@@ -23,7 +19,6 @@
 
 
 // Classes...
-
 class CGameSpyPlayer
 {
 	// Member functions...
@@ -32,30 +27,28 @@ public:
 	CGameSpyPlayer() { Clear(); }
 	~CGameSpyPlayer() { Term(); }
 
-	BOOL				Init(GServer pGServer, int nIndex);
-	void				Term();
+	BOOL	Init(GServer pGServer, int nIndex);
+	void	Term();
 
-	BOOL				IsInitialized() { return(m_bInit); }
+	BOOL	IsInitialized() { return(m_bInit); }
 
-	char*				GetName();
-	int					GetPing();
-	int					GetFrags();
-	void*				GetHandle() { return((void*)m_nIndex); }
+	char*	GetName();
+	int	GetPing();
+	int	GetFrags();
+	void*	GetHandle() { return((void*)m_nIndex); }
 
-	char*				GetStringValue(char* sKey, char* sDefault = "");
-	int					GetIntValue(char* sKey, int nDefault = 0);
-	float				GetFloatValue(char* sKey, float fDefault = 0);
+	char*	GetStringValue(char* sKey, char* sDefault = "");
+	int	GetIntValue(char* sKey, int nDefault = 0);
+	float	GetFloatValue(char* sKey, float fDefault = 0);
 
 protected:
-	void				Clear();
+	void	Clear();
 
 
 	// Member variables...
 
 private:
-	GServer				m_pGServer;
-	int					m_nIndex;
-	BOOL				m_bInit;
+	GServer	m_pGServer;
+	int	m_nIndex;
+	BOOL	m_bInit;
 };
-
-
