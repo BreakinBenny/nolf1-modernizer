1699074451f5ce10e8535d83d18d258e30c39f6c
BreakinBenny <fox@blaj.net>
2023-05-27 20:58:10 +0200

    Update GameSpyServer.h

--- LT2/lithshared/gamespy/gamespyclientmgr/GameSpyServer.h
+++ LT2/lithshared/gamespy/gamespyclientmgr/GameSpyServer.h
@@ -1,20 +1,14 @@
 /****************************************************************************
-;
-;	 MODULE:		GAMESPYSERVER (.H)
-;
-;	PURPOSE:		Game Spy Server Encapsulation Class
-;
-;	HISTORY:		08/01/00  [blg]  This file was created
-;
-;	COMMENT:		Copyright (c) 2000, LithTech, Inc.
-;
+;	 MODULE: GAMESPYSERVER (.H)
+;	PURPOSE: Game Spy Server Encapsulation Class
+;	HISTORY: 08/01/00  [blg]  This file was created
+;	COMMENT: Copyright (c) 2000, LithTech, Inc.
 ****************************************************************************/
 
 #pragma once
 
 
 // Includes...
-
 #ifdef GSCM_LOCAL_BUILD
 	#include "..\cengine\goaceng.h"
 	#include "gamespyplayer.h"
@@ -26,11 +20,11 @@
 
 // Defines...
 
-#define	GSS_MAX_PLAYERS		64
+#define	GSS_MAX_PLAYERS	64
 
-#define GSS_SORT_NAME		0
-#define GSS_SORT_PING		1
-#define GSS_SORT_FRAGS		2
+#define GSS_SORT_NAME	0
+#define GSS_SORT_PING	1
+#define GSS_SORT_FRAGS	2
 
 
 // Classes...
@@ -43,48 +37,45 @@ public:
 	CGameSpyServer() { Clear(); }
 	~CGameSpyServer() { Term(); }
 
-	BOOL				Init(GServer pServer);
-	void				Term();
+	BOOL	Init(GServer pServer);
+	void	Term();
 
-	BOOL				IsInitialized() { return(m_bInit); }
+	BOOL	IsInitialized() { return(m_bInit); }
 
-	char*				GetName();
-	char*				GetGameType();
-	int					GetPing();
-	int					GetNumPlayers();
-	int					GetMaxPlayers();
-	char*				GetMap();
-	char*				GetAddress();
-	void*				GetHandle() { return(m_pGServer); }
+	char*	GetName();
+	char*	GetGameType();
+	int	GetPing();
+	int	GetNumPlayers();
+	int	GetMaxPlayers();
+	char*	GetMap();
+	char*	GetAddress();
+	void*	GetHandle() { return(m_pGServer); }
 
-	char*				GetStringValue(char* sKey, char* sDefault = "");
-	int					GetIntValue(char* sKey, int nDefault = 0);
-	float				GetFloatValue(char* sKey, float fDefault = 0);
+	char*	GetStringValue(char* sKey, char* sDefault = "");
+	int	GetIntValue(char* sKey, int nDefault = 0);
+	float	GetFloatValue(char* sKey, float fDefault = 0);
 
-	int					UpdatePlayers();
+	int	UpdatePlayers();
 
-	CGameSpyPlayer*		GetPlayer(int nIndex);
-	CGameSpyPlayer*		GetFirstPlayer();
-	CGameSpyPlayer*		GetNextPlayer();
+	CGameSpyPlayer*	GetPlayer(int nIndex);
+	CGameSpyPlayer*	GetFirstPlayer();
+	CGameSpyPlayer*	GetNextPlayer();
 
-	void				SortPlayersByName(BOOL bAscending = TRUE) { SortPlayers(bAscending, GSS_SORT_NAME); }
-	void				SortPlayersByPing(BOOL bAscending = TRUE) { SortPlayers(bAscending, GSS_SORT_PING); }
-	void				SortPlayersByFrags(BOOL bAscending = FALSE) { SortPlayers(bAscending, GSS_SORT_FRAGS); }
+	void		SortPlayersByName(BOOL bAscending = TRUE) { SortPlayers(bAscending, GSS_SORT_NAME); }
+	void		SortPlayersByPing(BOOL bAscending = TRUE) { SortPlayers(bAscending, GSS_SORT_PING); }
+	void		SortPlayersByFrags(BOOL bAscending = FALSE) { SortPlayers(bAscending, GSS_SORT_FRAGS); }
 
 protected:
-	void				Clear();
-	void				SortPlayers(BOOL bAscending, int nKey);
+	void		Clear();
+	void		SortPlayers(BOOL bAscending, int nKey);
 
 
 	// Member variables...
 
 private:
-	GServer				m_pGServer;
-	BOOL				m_bInit;
-	CGameSpyPlayer		m_aPlayers[GSS_MAX_PLAYERS];
-	int					m_nNumPlayers;
-	int					m_nPlayerIndex;
+	GServer		m_pGServer;
+	BOOL		m_bInit;
+	CGameSpyPlayer	m_aPlayers[GSS_MAX_PLAYERS];
+	int		m_nNumPlayers;
+	int		m_nPlayerIndex;
 };
-
-
-
